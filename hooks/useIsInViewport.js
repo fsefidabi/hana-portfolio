eb4c6d703da6241e0fda9cca48c5692646e16785
Farzaneh Sefidabi <fsefidabi@gmail.com>
2023-09-04 16:03:03 +0330

    [feat] add navigation buttons for pdf viewer

---
+++ hooks/useIsInViewport.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+export function useIsInViewport(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+  const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting)
+    },
+    { rootMargin: "-400px" }
+  )
+
+
+  useEffect(() => {
+    observer?.observe(ref.current)
+
+    return () => {
+      observer?.disconnect()
+    }
+  }, [])
+
+  return isIntersecting
+}
