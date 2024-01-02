df0edd957b75c4f116e7d8a017d7ee85fd93be13
Farzaneh Sefidabi <fsefidabi@gmail.com>
2024-01-02 09:33:36 +0330

    [fix] revert imageCarousel styles

--- components/atoms/ImageCarousel/SlideshowButtons.js
+++ components/atoms/ImageCarousel/SlideshowButtons.js
@@ -13,9 +13,9 @@ function SlideshowButtons(props) {
             className={`${styles.slideshowButtons} ${isZoomed ? styles.largeSlideshowButtons : styles.smallSlideshowButtons}`}>
             <AnimatePresence>
                 <motion.div
-                    className={`__link ${styles.buttonWrapper} ${(mousePositionOverSlider === ALIGNMENT.LEFT || (isSlidable && isZoomed)) ? "flex" : "flex"}`}
+                    className={`__link ${styles.buttonWrapper} ${(mousePositionOverSlider === ALIGNMENT.LEFT || (isSlidable && isZoomed)) ? "flex" : "hidden"}`}
                     variants={fade.fadeIn(0.5)}
-                    animate={(mousePositionOverSlider === ALIGNMENT.LEFT || (isSlidable && isZoomed)) ? "animate" : "animate"}
+                    animate={(mousePositionOverSlider === ALIGNMENT.LEFT || (isSlidable && isZoomed)) ? "animate" : "initial"}
                     onClick={handleShowPrevSlide}
                 >
                     <button type="button">
@@ -26,9 +26,9 @@ function SlideshowButtons(props) {
 
             <AnimatePresence>
                 <motion.div
-                    className={`__link ${styles.buttonWrapper} ${(mousePositionOverSlider === ALIGNMENT.RIGHT || (isSlidable && isZoomed)) ? "flex" : "flex"}`}
+                    className={`__link ${styles.buttonWrapper} ${(mousePositionOverSlider === ALIGNMENT.RIGHT || (isSlidable && isZoomed)) ? "flex" : "hidden"}`}
                     variants={fade.fadeIn(0.5)}
-                    animate={(mousePositionOverSlider === ALIGNMENT.RIGHT || (isSlidable && isZoomed)) ? "animate" : "animate"}
+                    animate={(mousePositionOverSlider === ALIGNMENT.RIGHT || (isSlidable && isZoomed)) ? "animate" : "initial"}
                     onClick={handleShowNextSlide}
                 >
                     <button type="button">
