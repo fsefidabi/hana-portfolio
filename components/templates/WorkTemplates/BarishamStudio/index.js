9bd7dab5f97866475eb3a2201cdaf235a7a69240
Farzaneh Sefidabi <fsefidabi@gmail.com>
2024-06-14 10:35:27 +0330

    [refactor] move barisham-studio project detail and images from sanity to the project source

--- components/templates/WorkTemplates/BarishamStudio/index.js
+++ components/templates/WorkTemplates/BarishamStudio/index.js
@@ -4,8 +4,9 @@ import { textReveal } from "framerMotionAnimations"
 import ImageCarousel from "components/atoms/ImageCarousel"
 import MotionTextRevealTitle from "components/atoms/MotionTextRevealTitle"
 import styles from "../workTemplates.module.css"
+import work from "constants/data/works/barisham-studio.json"
 
-function BarishamStudioWorkTemplate({ work, clickPosition, zoomedSize, handleImageClick }) {
+function BarishamStudioWorkTemplate({ clickPosition, zoomedSize, handleImageClick }) {
     return (
         <div className={`contentWrapper mt-16 ${styles.container}`}>
             <MotionTextRevealTitle
